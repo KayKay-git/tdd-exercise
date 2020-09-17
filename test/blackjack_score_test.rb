f4a1764742c199e448786668930d1aeab100b758
KayKay-git <ada@Adas-MBP.attlocal.net>
2020-09-16 19:40:49 -0700

    Completed Test

--- test/blackjack_score_test.rb
+++ test/blackjack_score_test.rb
@@ -18,26 +18,58 @@ describe 'Blackjack Score' do
     score = blackjack_score(hand)
 
     # Assert <-  You do this part!
+    expect(score).must_equal 7
 
   end
 
   it 'facecards have values calculated correctly' do
+    # Arrange
+    hand = ['Jack','Queen']
+
+    # Act
+    score = blackjack_score(hand)
+
+    # Assert
+    expect(score).must_equal 20
+
 
   end
 
   it 'calculates aces as 11 where it does not go over 21' do
 
+    # Arrange
+    hand = [10, "Ace"]
+    # Act
+    score = blackjack_score(hand)
+    # Assert
+    expect(score).must_equal 21
+
   end
 
   it 'calculates aces as 1, if an 11 would cause the score to go over 21' do
+    # Arrange
+    hand = [10, 5, "Ace"]
+    # Act
+    score = blackjack_score(hand)
+    # Assert
+    expect(score).must_equal 16
+
 
   end
 
   it 'raises an ArgumentError for invalid cards' do
+    # Arrange
+    hand = [10, "wild"]
+
+      expect { blackjack_score(hand) }.must_raise ArgumentError
 
   end
 
   it 'raises an ArgumentError for scores over 21' do
+    # Arrange
+    hand = [20,5]
 
+    expect { blackjack_score(hand) }.must_raise ArgumentError
   end
+
 end
