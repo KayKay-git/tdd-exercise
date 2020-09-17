69094b0cf451483366daab2bccd31b2d991fe24b
KayKay-git <ada@Adas-MBP.attlocal.net>
2020-09-16 19:36:58 -0700

    Completed

--- lib/blackjack_score.rb
+++ lib/blackjack_score.rb
@@ -2,6 +2,42 @@
 
 VALID_CARDS = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace']
 
+
+cards_in_hand = VALID_CARDS.sample(2)
+pp cards_in_hand
+
+
 def blackjack_score(hand)
 
+  score = 0
+
+  hand.each do |item|
+
+    if VALID_CARDS.include?(item)
+      case item
+      when "Jack", "Queen", "King"
+        score += 10
+      when Integer
+        score += item
+      when "Ace"
+        if score <= 10
+          score += 11
+        elsif
+        score += 1
+        end
+      end
+    else
+      raise ArgumentError.new("You have an Invalid card")
+    end
+  end
+
+  if score > 21
+    raise ArgumentError.new("You lose")
+  end
+    return score
+  
 end
+
+
+pp blackjack_score(cards_in_hand)
+
